ffcb02cfc5c07450c05d879603d619821d1b88bc
Tanja Olsson <tanja.olsson@skola.botkyrka.sem>
2020-11-09 16:00:11 +0100

    lektion

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ const MIN_NUMBER = 0
 
 let guess
 
+const results = []
+
+function randomInt(n) {
+    return Math.floor(Math.random() * n) + 1
+    
+}
+
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
+    
+    guess = randomInt(MAX_NUMBER)
+    setMessage(`Is it ${guess}?`)
 
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
+    guess++
+    setMessage(`Is it ${guess}?`)
     
 })
-
-
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    // Vad ska hända när man tryckt too low?
+   
+})
 function setMessage(msg) {
     document.getElementById('message').innerText = msg
 }
@@ -22,9 +36,8 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
-
-    // setMessage(`Is it ${guess}?`)
+    guess = 50
+    setMessage(`Is it ${guess}?`)
 }
 
 document
